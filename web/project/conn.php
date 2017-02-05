1f6662e6cf756e901fef219ebdf251eaa1d75a60
ryan brown <r_yan@outlook.com>
2017-02-04 23:07:12 -0600

    week 6 3

--- web/project/conn.php
+++ web/project/conn.php
@@ -16,7 +16,7 @@ error_reporting(E_ALL);
 //}
 //Heroku
  //default Heroku Postgres configuration URL
-$dbUrl = getenv('pg_dump postgres://geykwcflfainbt:926d0933a74d94512ae583b6cf23489b4812e1bdf2c9289c122c85fbf14eef6f@ec2-107-20-193-74.compute-1.amazonaws.com:5432/d2je6okqu4jjr');
+$dbUrl = getenv('postgres://geykwcflfainbt:926d0933a74d94512ae583b6cf23489b4812e1bdf2c9289c122c85fbf14eef6f@ec2-107-20-193-74.compute-1.amazonaws.com:5432/d2je6okqu4jjr');
 
 if (empty($dbUrl)) {
     // example localhost configuration URL with postgres username and a database called cs313db
